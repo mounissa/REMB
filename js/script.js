b6640a0c2a6f6b5ee5ea5992834503a3cd919b01
ali <ali@live.fr>
2016-04-13 16:29:54 +0200

    ajout header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,45 @@
  $(document).ready(function() {
-			
+	 $('#dialog').dialog({
+		   autoOpen: false,
+		   width: "auto",
+		   modal: true,
+		   show: 'fadeIn(3000)'
+		 
+	 });
+	 $('#dialog').dialog("close",function(){
+		  /*$("body").css({
+		        background:"none"
+		       
+		   });*/
+		 console.log("test");
+	 });
+	  $( "#connexion" ).click(function( event ) {
+	      // cette ligne est très importante pour empêcher les liens ou les boutons de rediriger
+	      // vers une autre page avant que l'usager ait cliqué dans le popup
+	      event.preventDefault();
+	      $("body").css({
+		        background:"rgb(0, 0, 0)",
+		        opacity: ".10 !important",
+		        filter: "Alpha(Opacity=10)",
+		   });
+	      // affichage du popup
+	      $( "#dialog" ).dialog( "open" );
+		
+	  });
+	  $( "#connexion-small" ).click(function( event ) {
+	      // cette ligne est très importante pour empêcher les liens ou les boutons de rediriger
+	      // vers une autre page avant que l'usager ait cliqué dans le popup
+	      event.preventDefault();
+	      $("body").css({
+		        background:"rgb(0, 0, 0)",
+		        opacity: ".10 !important",
+		        filter: "Alpha(Opacity=10)",
+		   });
+	      // affichage du popup
+	      $( "#dialog" ).dialog( "open" );
+		
+	  });
+	 
 			$('#calendar').fullCalendar({
 				lang:'fr',
 				header: {
@@ -28,5 +68,7 @@
 					}
 				]
 			});
+			
+			
 			
 });
